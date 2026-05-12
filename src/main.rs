c549c23e3da48e92a6cc32c18dd7f55f43717485
cloudthegoat <awan.panjer@gmail.com>
2026-05-12 17:14:03 +0700

    Fix RabbitMQ client runtime with lapin

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,74 @@
 use borsh::{BorshDeserialize, BorshSerialize};
-use crosstown_bus::{CrosstownBus, HandleError, MessageHandler};
+use lapin::{
+    options::{BasicPublishOptions, QueueDeclareOptions},
+    types::FieldTable,
+    BasicProperties, Connection, ConnectionProperties,
+};
+
 #[derive(Debug, Clone, BorshDeserialize, BorshSerialize)]
 pub struct UserCreatedEventMessage {
     pub user_id: String,
     pub user_name: String,
 }
 
-pub struct UserCreatedHandler;
-impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
-    fn handle(&self, message: Box<UserCreatedEventMessage>) -> Result<(), HandleError> {
-        println!("Message received on handler 1: {:?}", message);
-        Ok(())
-    }
-}
-fn main() {
-    let mut p =
-        CrosstownBus::new_queue_publisher("amqp://guest:guest@localhost:5672".to_owned()).unwrap();
-    _ = p.publish_event(
-        "user_created".to_owned(),
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let options = ConnectionProperties::default()
+        .with_executor(tokio_executor_trait::Tokio::current())
+        .with_reactor(async_reactor_trait::AsyncIo);
+
+    let connection = Connection::connect("amqp://guest:guest@127.0.0.1:5672", options).await?;
+    let channel = connection.create_channel().await?;
+
+    channel
+        .queue_declare(
+            "user_created",
+            QueueDeclareOptions {
+                durable: false,
+                auto_delete: false,
+                ..QueueDeclareOptions::default()
+            },
+            FieldTable::default(),
+        )
+        .await?;
+
+    let messages = [
         UserCreatedEventMessage {
             user_id: "1".to_owned(),
             user_name: "2406420772-Amir".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "2".to_owned(),
             user_name: "2406420772-Budi".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "3".to_owned(),
             user_name: "2406420772-Cica".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "4".to_owned(),
             user_name: "2406420772-Dira".to_owned(),
         },
-    );
-    _ = p.publish_event(
-        "user_created".to_owned(),
         UserCreatedEventMessage {
             user_id: "5".to_owned(),
             user_name: "2406420772-Emir".to_owned(),
         },
-    );
+    ];
+
+    for message in messages {
+        let payload = message.try_to_vec()?;
+        channel
+            .basic_publish(
+                "",
+                "user_created",
+                BasicPublishOptions::default(),
+                &payload,
+                BasicProperties::default(),
+            )
+            .await?
+            .await?;
+    }
+
+    println!("Published 5 user_created events.");
+    Ok(())
 }
